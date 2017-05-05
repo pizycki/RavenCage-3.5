6a2e9da1a02b5f8e2ec4a63d573aac2d7dde07a4
Paweł Iżycki <pawelizycki@gmail.com>
2017-05-05 20:14:45 +0200

    Add missing 'latest' variable in publishing

--- Publish-Image.ps1
+++ Publish-Image.ps1
@@ -4,18 +4,19 @@ Import-Module .\Docker-Module.psm1 3>$null
 [boolean] $pushToDockerHub   = [System.Convert]::ToBoolean((Get-EnvVariable "PUSH_TO_DOCKERHUB"))
 [string]  $repository        = Get-EnvVariable "DOCKER_REPOSITORY"
 [string]  $tag               = Get-EnvVariable "APPVEYOR_REPO_TAG_NAME"
+[boolean] $latest            = [System.Convert]::ToBoolean((Get-EnvVariable "IS_LATEST"))
 [string]  $dockerHubUser     = Get-EnvVariable "DOCKERHUB_USER"
 [string]  $dockerHubPassword = Get-EnvVariable "DOCKERHUB_PASSWORD"
 
-Write-Frame "Publishing image to DockerHub" Magenta
-
-# Publish on DockerHub, according to build settings.
 if ( $pushToDockerHub ) {
+
+    Write-Frame "Publishing image to DockerHub" Magenta
 
     Write-Host "Publishing '${repository}:${tag}'." 
     Push-ImageToDockerHub $repository $tag $dockerHubUser $dockerHubPassword
     Write-Success "Image '${repository}:${tag}' has been published!"
 
+    Write-Host "Is latest? ${latest}"
     if ( $latest ) {
         Write-Host "Publishing '${repository}:${tag}' as 'latest'" 
         Push-ImageToDockerHub $repository "latest" $dockerHubUser $dockerHubPassword
